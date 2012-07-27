7dd4b9fc337eb5c582c7ea53c8dc4e8256d27b15
marpet <marco.peters@brockmann-consult.de>
2012-07-27 14:41:25 +0200

    added some parameter test

--- lc-aggregation-tool/src/main/java/org/esa/cci/lc/aggregation/AggregationOp.java
+++ lc-aggregation-tool/src/main/java/org/esa/cci/lc/aggregation/AggregationOp.java
@@ -24,16 +24,16 @@ import org.esa.beam.framework.gpf.annotations.SourceProduct;
         description = "Allows to re-project, aggregate and subset LC map and conditions products.")
 public class AggregationOp extends Operator {
 
-    @SourceProduct(description = "LC CCI map or conditions product")
+    @SourceProduct(description = "LC CCI map or conditions product", optional = false)
     Product source;
 
     @Parameter(description = "Defines the projection method for the target product.",
                valueSet = {"GAUSSIAN_GRID", "GEOGRAPHIC_LAT_LON", "ROTATED_LAT_LON"}, defaultValue = "GAUSSIAN_GRID")
     ProjectionMethod projectionMethod;
 
-    @Parameter(description = "Size of a pixel in X-direction in degree.", defaultValue = "0.05", unit = "°")
+    @Parameter(description = "Size of a pixel in X-direction in degree.", defaultValue = "0.1", unit = "°")
     double pixelSizeX;
-    @Parameter(description = "Size of a pixel in Y-direction in degree.", defaultValue = "0.05", unit = "°")
+    @Parameter(description = "Size of a pixel in Y-direction in degree.", defaultValue = "0.1", unit = "°")
     double pixelSizeY;
 
     @Parameter(description = "The western longitude.", interval = "[-180,180]", defaultValue = "-15.0", unit = "°")
@@ -45,13 +45,14 @@ public class AggregationOp extends Operator {
     @Parameter(description = "The southern latitude.", interval = "[-90,90]", defaultValue = "35.0", unit = "°")
     double southBound;
 
-    @Parameter(description = "Whether or not to add majority classes and the fractional area to the output.")
+    @Parameter(description = "Whether or not to add majority classes and the fractional area to the output.",
+               defaultValue = "true")
     boolean outputMajorityClasses;
     // todo (mp, 26.07.12) - set value range if max. classes is fixed or add validator which uses input to define maximum.
     @Parameter(description = "The number of majority classes generated and added to the output.", defaultValue = "5")
     int numberOfMajorityClasses;
 
-    @Parameter(description = "Whether or not to add PFT classes to the output.")
+    @Parameter(description = "Whether or not to add PFT classes to the output.", defaultValue = "true")
     boolean outputPFTClasses;
     // todo (mp, 26.07.12) - set value range if max. classes is fixed or add validator which uses input to define maximum.
     // todo (mp, 26.07.12) - meaningful default value?
@@ -60,6 +61,109 @@ public class AggregationOp extends Operator {
 
     @Override
     public void initialize() throws OperatorException {
+        validateParameters();
+    }
+
+    ProjectionMethod getProjectionMethod() {
+        return projectionMethod;
+    }
+
+    void setProjectionMethod(ProjectionMethod projectionMethod) {
+        this.projectionMethod = projectionMethod;
+    }
+
+    double getPixelSizeX() {
+        return pixelSizeX;
+    }
+
+    void setPixelSizeX(double pixelSizeX) {
+        this.pixelSizeX = pixelSizeX;
+    }
+
+    double getPixelSizeY() {
+        return pixelSizeY;
+    }
+
+    void setPixelSizeY(double pixelSizeY) {
+        this.pixelSizeY = pixelSizeY;
+    }
+
+    double getWestBound() {
+        return westBound;
+    }
+
+    void setWestBound(double westBound) {
+        this.westBound = westBound;
+    }
+
+    double getNorthBound() {
+        return northBound;
+    }
+
+    void setNorthBound(double northBound) {
+        this.northBound = northBound;
+    }
+
+    double getEastBound() {
+        return eastBound;
+    }
+
+    void setEastBound(double eastBound) {
+        this.eastBound = eastBound;
+    }
+
+    double getSouthBound() {
+        return southBound;
+    }
+
+    void setSouthBound(double southBound) {
+        this.southBound = southBound;
+    }
+
+    boolean isOutputMajorityClasses() {
+        return outputMajorityClasses;
+    }
+
+    void setOutputMajorityClasses(boolean outputMajorityClasses) {
+        this.outputMajorityClasses = outputMajorityClasses;
+    }
+
+    int getNumberOfMajorityClasses() {
+        return numberOfMajorityClasses;
+    }
+
+    void setNumberOfMajorityClasses(int numberOfMajorityClasses) {
+        this.numberOfMajorityClasses = numberOfMajorityClasses;
+    }
+
+    boolean isOutputPFTClasses() {
+        return outputPFTClasses;
+    }
+
+    void setOutputPFTClasses(boolean outputPFTClasses) {
+        this.outputPFTClasses = outputPFTClasses;
+    }
+
+    int getNumberOfPFTClasses() {
+        return numberOfPFTClasses;
+    }
+
+    void setNumberOfPFTClasses(int numberOfPFTClasses) {
+        this.numberOfPFTClasses = numberOfPFTClasses;
+    }
+
+
+    private void validateParameters() {
+        if (westBound >= eastBound) {
+            throw new OperatorException("West bound must be western of east bound.");
+        }
+        if (northBound <= southBound) {
+            throw new OperatorException("North bound must be northern of south bound.");
+        }
+        if (!outputMajorityClasses && !outputPFTClasses) {
+            throw new OperatorException("Nothing to process. Majority classes and/or " +
+                                        "PFT classes must be selected for output.");
+        }
     }
 
     public enum ProjectionMethod {
---
+++ lc-aggregation-tool/src/test/java/org/esa/cci/lc/aggregation/AggregationOpTest.java
@@ -0,0 +1,87 @@
+package org.esa.cci.lc.aggregation;
+
+import org.esa.beam.framework.gpf.GPF;
+import org.esa.beam.framework.gpf.OperatorException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * @author Marco Peters
+ */
+public class AggregationOpTest {
+
+    private static AggregationOp.Spi aggregationSpi;
+
+    @BeforeClass
+    public static void beforeClass() {
+        aggregationSpi = new AggregationOp.Spi();
+        GPF.getDefaultInstance().getOperatorSpiRegistry().addOperatorSpi(aggregationSpi);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        GPF.getDefaultInstance().getOperatorSpiRegistry().removeOperatorSpi(aggregationSpi);
+    }
+
+    @Test
+    public void testDefaultValues() {
+        AggregationOp aggrOp = (AggregationOp) aggregationSpi.createOperator();
+        assertEquals(AggregationOp.ProjectionMethod.GAUSSIAN_GRID, aggrOp.getProjectionMethod());
+        assertEquals(0.1, aggrOp.getPixelSizeX(), 1.0e-8);
+        assertEquals(0.1, aggrOp.getPixelSizeY(), 1.0e-8);
+        assertEquals(-15.0, aggrOp.getWestBound(), 1.0e-8);
+        assertEquals(30.0, aggrOp.getEastBound(), 1.0e-8);
+        assertEquals(75.0, aggrOp.getNorthBound(), 1.0e-8);
+        assertEquals(35.0, aggrOp.getSouthBound(), 1.0e-8);
+        assertTrue(aggrOp.isOutputMajorityClasses());
+        assertEquals(5, aggrOp.getNumberOfMajorityClasses());
+        assertTrue(aggrOp.isOutputPFTClasses());
+        assertEquals(3, aggrOp.getNumberOfPFTClasses());
+    }
+
+    @Test
+    public void testWestEastBound() {
+        AggregationOp aggrOp = (AggregationOp) aggregationSpi.createOperator();
+        aggrOp.setWestBound(10);
+        aggrOp.setEastBound(3);
+        try {
+            aggrOp.initialize();
+        } catch (OperatorException oe) {
+            String message = oe.getMessage().toLowerCase();
+            assertTrue(message.contains("west bound"));
+            assertTrue(message.contains("east bound"));
+        }
+    }
+
+    @Test
+    public void testNorthSouthBound() {
+        AggregationOp aggrOp = (AggregationOp) aggregationSpi.createOperator();
+        aggrOp.setNorthBound(30);
+        aggrOp.setSouthBound(70);
+        try {
+            aggrOp.initialize();
+        } catch (OperatorException oe) {
+            String message = oe.getMessage().toLowerCase();
+            assertTrue(message.contains("north bound"));
+            assertTrue(message.contains("south bound"));
+        }
+    }
+
+    @Test
+    public void testNoOutputClassesSelected() {
+        AggregationOp aggrOp = (AggregationOp) aggregationSpi.createOperator();
+        aggrOp.setOutputMajorityClasses(false);
+        aggrOp.setOutputPFTClasses(false);
+        try {
+            aggrOp.initialize();
+        } catch (OperatorException oe) {
+            String message = oe.getMessage().toLowerCase();
+            assertTrue(message.contains("and/or"));
+            assertTrue(message.contains("classes"));
+        }
+    }
+}
