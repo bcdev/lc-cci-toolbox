1f02b5b880a32072cd41da91d37bd1fb06a867a9
marpet <marco.peters@brockmann-consult.de>
2012-09-03 16:14:26 +0200

    generalised test

--- lc-aggregation-tool/src/test/java/org/esa/cci/lc/aggregation/LcAggregatorTest.java
+++ lc-aggregation-tool/src/test/java/org/esa/cci/lc/aggregation/LcAggregatorTest.java
@@ -1,14 +1,7 @@
 package org.esa.cci.lc.aggregation;
 
 import org.esa.beam.binning.support.VariableContextImpl;
-import org.esa.beam.framework.datamodel.Band;
-import org.esa.beam.framework.datamodel.CrsGeoCoding;
-import org.esa.beam.framework.datamodel.Product;
-import org.esa.beam.framework.datamodel.ProductData;
-import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.junit.Test;
-
-import javax.media.jai.operator.ConstantDescriptor;
 
 import static org.junit.Assert.*;
 
@@ -32,10 +25,12 @@ public class LcAggregatorTest {
         assertTrue(Float.isNaN(aggregator.getOutputFillValue()));
 
         assertEquals("class_area_1", spatialFeatureNames[0]);
-        assertEquals("class_area_24", spatialFeatureNames[LcAggregatorDescriptor.NUM_LC_CLASSES - 1]);
+        assertEquals("class_area_" + LcAggregatorDescriptor.NUM_LC_CLASSES,
+                     spatialFeatureNames[LcAggregatorDescriptor.NUM_LC_CLASSES - 1]);
 
         assertEquals("class_area_1", outputFeatureNames[0]);
-        assertEquals("class_area_24", outputFeatureNames[LcAggregatorDescriptor.NUM_LC_CLASSES - 1]);
+        assertEquals("class_area_" + LcAggregatorDescriptor.NUM_LC_CLASSES,
+                     outputFeatureNames[LcAggregatorDescriptor.NUM_LC_CLASSES - 1]);
         assertEquals("majority_class_1", outputFeatureNames[LcAggregatorDescriptor.NUM_LC_CLASSES]);
         assertEquals("majority_class_3",
                      outputFeatureNames[LcAggregatorDescriptor.NUM_LC_CLASSES + numMajorityClasses - 1]);
@@ -46,13 +41,5 @@ public class LcAggregatorTest {
         LcAggregatorDescriptor lcAggregatorDescriptor = new LcAggregatorDescriptor();
         LcAggregatorConfig config = new LcAggregatorConfig("classes", numMajorityClasses);
         return (LcAggregator) lcAggregatorDescriptor.createAggregator(varCtx, config);
-    }
-
-    private Product createSourceProduct() throws Exception {
-        Product product = new Product("P", "T", 360, 180);
-        Band classesBand = product.addBand("classes", ProductData.TYPE_UINT8);
-        classesBand.setSourceImage(ConstantDescriptor.create(360f, 180f, new Byte[]{10}, null));
-        product.setGeoCoding(new CrsGeoCoding(DefaultGeographicCRS.WGS84, 360, 180, -180.0, 90.0, 1.0, 1.0));
-        return product;
     }
 }
