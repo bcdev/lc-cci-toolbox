4d12b36413ac8ec070874ec98c84e9b6642d2577
marpet <marco.peters@brockmann-consult.de>
2012-09-05 16:53:20 +0200

    now using a map for getting the index vector by class value

--- lc-aggregation-tool/src/main/java/org/esa/cci/lc/aggregation/LcAggregator.java
+++ lc-aggregation-tool/src/main/java/org/esa/cci/lc/aggregation/LcAggregator.java
@@ -6,30 +6,47 @@ import org.esa.beam.binning.Observation;
 import org.esa.beam.binning.Vector;
 import org.esa.beam.binning.WritableVector;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Marco Peters
  */
 public class LcAggregator extends AbstractAggregator {
 
-    private final int[] classToSpatialVectorIndex;
+    // this Lut is for the LC example
+    private static Map<Integer, Integer> classValueToVectorIndexMap;
+
+    static {
+        classValueToVectorIndexMap = new HashMap<Integer, Integer>();
+        classValueToVectorIndexMap.put(10, 0);
+        classValueToVectorIndexMap.put(20, 1);
+        classValueToVectorIndexMap.put(30, 2);
+        classValueToVectorIndexMap.put(40, 3);
+        classValueToVectorIndexMap.put(50, 4);
+        classValueToVectorIndexMap.put(60, 5);
+        classValueToVectorIndexMap.put(70, 6);
+        classValueToVectorIndexMap.put(80, 7);
+        classValueToVectorIndexMap.put(90, 8);
+        classValueToVectorIndexMap.put(100, 9);
+        classValueToVectorIndexMap.put(110, 10);
+        classValueToVectorIndexMap.put(120, 11);
+        classValueToVectorIndexMap.put(130, 12);
+        classValueToVectorIndexMap.put(140, 13);
+        classValueToVectorIndexMap.put(150, 14);
+        classValueToVectorIndexMap.put(160, 15);
+        classValueToVectorIndexMap.put(170, 16);
+        classValueToVectorIndexMap.put(180, 17);
+        classValueToVectorIndexMap.put(190, 18);
+        classValueToVectorIndexMap.put(200, 19);
+        classValueToVectorIndexMap.put(210, 20);
+        classValueToVectorIndexMap.put(220, 21);
+        classValueToVectorIndexMap.put(230, 22);
+        classValueToVectorIndexMap.put(255, 23);
+    }
 
     LcAggregator(String[] spatialFeatureNames, String[] outputFeatureNames) {
         super(LcAggregatorDescriptor.NAME, spatialFeatureNames, spatialFeatureNames, outputFeatureNames, null);
-
-        // this Lut is for the LC example
-        classToSpatialVectorIndex = new int[]{
-                -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1,
-                -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, 4, -1,
-                -1, -1, -1, -1, -1, -1, -1, -1, 5, -1, -1, -1, -1, -1, -1, -1, -1, -1, 6, -1, -1, -1, -1, -1, -1, -1,
-                -1, -1, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, 8, -1, -1, -1, -1, -1, -1, -1, -1, -1, 9, -1, -1, -1,
-                -1, -1, -1, -1, -1, -1, 10, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, -1, -1, -1, -1, -1, -1, -1, -1,
-                -1, 12, -1, -1, -1, -1, -1, -1, -1, -1, -1, 13, -1, -1, -1, -1, -1, -1, -1, -1, -1, 14, -1, -1, -1,
-                -1, -1, -1, -1, -1, -1, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, 16, -1, -1, -1, -1, -1, -1, -1, -1,
-                -1, 17, -1, -1, -1, -1, -1, -1, -1, -1, -1, 18, -1, -1, -1, -1, -1, -1, -1, -1, -1, 19, -1, -1, -1,
-                -1, -1, -1, -1, -1, -1, 20, -1, -1, -1, -1, -1, -1, -1, -1, -1, 21, -1, -1, -1, -1, -1, -1, -1, -1,
-                -1, 22, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-                -1, 23
-        };
     }
 
     @Override
@@ -44,10 +61,13 @@ public class LcAggregator extends AbstractAggregator {
         Observation observation = (Observation) observationVector;
         // todo: estimate area for input observation latitude assuming it is the center of a LC-map pixel
         float area = 1.0F;
-        int classIndex = (int) observation.get(0);
-        // map classIndex to spatial vector index i
-        int i = classToSpatialVectorIndex[classIndex];
-        int index = Math.abs(i) % LcAggregatorDescriptor.NUM_LC_CLASSES;
+        int classValue = (int) observation.get(0);
+        // map classValue to spatial vector index i
+        Integer vectorIndex = classValueToVectorIndexMap.get(classValue);
+        if (vectorIndex == null) { // how to handle a value which is not assigned to a class
+            vectorIndex = 1;
+        }
+        int index = vectorIndex % LcAggregatorDescriptor.NUM_LC_CLASSES;
         float oldValue = spatialVector.get(index);
         if (Float.isNaN(oldValue)) {
             oldValue = 0.0f;
