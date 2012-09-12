6d7edccc1580878c4d4e3e0ff72b5733f97b6b8f
marpet <marco.peters@brockmann-consult.de>
2012-09-12 18:35:11 +0200

    made test more understandable started to extend test

--- lc-aggregation-tool/src/test/java/org/esa/cci/lc/aggregation/FractionalAreaCalculatorTest.java
+++ lc-aggregation-tool/src/test/java/org/esa/cci/lc/aggregation/FractionalAreaCalculatorTest.java
@@ -12,17 +12,20 @@ public class FractionalAreaCalculatorTest {
 
     @Test
     public void testCalculate_WhenGridAndMapHaveSameSize() throws Exception {
-        FractionalAreaCalculator areaCalculator = new FractionalAreaCalculator(SEAGrid.RE, 2160, 4320, 2160);
+        FractionalAreaCalculator fractionCalculator = new FractionalAreaCalculator(SEAGrid.RE, 2160, 4320, 2160);
         double area;
 
-        area = areaCalculator.calculate(0, 0, 4320);
-        assertEquals(4320.0 / 4320.0, area, 1.0e-6);
+        // observation has the same size as the bin
+        area = fractionCalculator.calculate(0, 0, 4320);
+        assertEquals(1.0 / 1.0, area, 1.0e-6);
 
-        area = areaCalculator.calculate(90, 90, 3);
-        assertEquals(3.0 / 4320.0, area, 1.0e-6);
+        // 1440 (third of 4320) observations are covered by one bin
+        area = fractionCalculator.calculate(90, 90, 3);
+        assertEquals(1.0 / 1440.0, area, 1.0e-6);
 
-        area = areaCalculator.calculate(-90, -90, 3);
-        assertEquals(3.0 / 4320.0, area, 1.0e-6);
+        // 1440 (third of 4320) observations are covered by one bin
+        area = fractionCalculator.calculate(-90, -90, 3);
+        assertEquals(1.0 / 1440.0, area, 1.0e-6);
     }
 
     @Test
@@ -33,10 +36,29 @@ public class FractionalAreaCalculatorTest {
         area = areaCalculator.calculate(0, 0, 4320);
         assertEquals(4320.0 / 7000.0 * 4320.0 / 7000.0, area, 1.0e-6);
 
+        // 3 bins cover 7000 pixels in width
+        // one bin height covers one-2160th of pole to pole distance and one observation height covers one-3500th of
+        // pole to pole distance  --> one observation covers 2160/3500 of one bin in height
         area = areaCalculator.calculate(90, 90, 3);
-        assertEquals(4320.0 / 7000.0 * 3.0 / 7000.0, area, 1.0e-6);
+        assertEquals(3.0 / 7000.0 * 2160.0 / 3500.0, area, 1.0e-6);
 
         area = areaCalculator.calculate(-90, -90, 3);
-        assertEquals(4320.0 / 7000.0 * 3.0 / 7000.0, area, 1.0e-6);
+        assertEquals(3.0 / 7000.0 * 2160.0 / 3500.0, area, 1.0e-6);
+    }
+
+    @Test
+    public void testThatFractionalInputPixelsAreConsidered() throws Exception {
+        FractionalAreaCalculator fractionCalculator = new FractionalAreaCalculator(SEAGrid.RE, 10, 20, 10);
+        // First row has 3 bins
+        SEAGrid seaGrid = new SEAGrid(10);
+
+        // retrieve lat/lon of first bin
+        double binLat = seaGrid.getCenterLat(0);
+        double binLon = seaGrid.getCenterLon(0, 0);
+
+        // todo - longitude of the observation is missing
+        double fraction = fractionCalculator.calculate(binLat, binLat, 3);
+        assertEquals(1.0 / (20.0 / 3.0), fraction, 1.0e-6);
+
     }
 }
