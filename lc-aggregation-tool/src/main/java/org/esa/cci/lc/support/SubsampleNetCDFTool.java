04ef56c5bf646a780e5f097fc8f2174f3aaaf154
marpet <marco.peters@brockmann-consult.de>
2013-04-23 15:42:40 +0200

    added SubsampleNetCDFTool

---
+++ lc-aggregation-tool/src/main/java/org/esa/cci/lc/support/SubsampleNetCDFTool.java
@@ -0,0 +1,33 @@
+package org.esa.cci.lc.support;
+
+import org.esa.beam.framework.dataio.ProductIO;
+import org.esa.beam.framework.dataio.ProductIOPlugInManager;
+import org.esa.beam.framework.dataio.ProductSubsetDef;
+import org.esa.beam.framework.datamodel.Product;
+import org.esa.cci.lc.conversion.LcMapNetCdf4WriterPlugIn;
+
+import java.io.IOException;
+
+/**
+ * Creates a subsampled LC-NetCDF file from the given LC-NetCDF file.
+ * Just a tool used during development.
+ *
+ * @author Marco Peters
+ */
+public class SubsampleNetCDFTool {
+
+    public static void main(String[] args) throws IOException {
+        Product product = ProductIO.readProduct(args[0]);
+        ProductSubsetDef productSubsetDef = new ProductSubsetDef();
+        int subSamplingY = product.getSceneRasterHeight() / Integer.parseInt(args[1]);
+        int subSamplingX = subSamplingY;
+        productSubsetDef.setSubSampling(subSamplingX, subSamplingY);
+        String productName = product.getName() + "_subset_" + args[1];
+        productSubsetDef.setSubsetName(productName);
+        Product subsetProduct = product.createSubset(productSubsetDef, productName, product.getDescription());
+        LcMapNetCdf4WriterPlugIn lcMapNetCdf4WriterPlugIn = new LcMapNetCdf4WriterPlugIn();
+        ProductIOPlugInManager.getInstance().addWriterPlugIn(lcMapNetCdf4WriterPlugIn);
+        String formatName = lcMapNetCdf4WriterPlugIn.getFormatNames()[0];
+        ProductIO.writeProduct(subsetProduct, "./" + productName, formatName);
+    }
+}
