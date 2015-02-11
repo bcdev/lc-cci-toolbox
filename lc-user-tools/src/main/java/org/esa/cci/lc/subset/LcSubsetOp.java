26cd478430e5385f2fd4adec64e009b4986d910a
marpet <marco.peters@brockmann-consult.de>
2015-02-11 15:34:35 +0100

    updated the subsetOp to handle region parameter better

--- lc-user-tools/src/main/java/org/esa/cci/lc/subset/LcSubsetOp.java
+++ lc-user-tools/src/main/java/org/esa/cci/lc/subset/LcSubsetOp.java
@@ -16,6 +16,9 @@ import org.esa.cci.lc.io.LcMapMetadata;
 import org.esa.cci.lc.io.LcMapNetCdf4WriterPlugIn;
 import org.esa.cci.lc.io.LcWbNetCdf4WriterPlugIn;
 import org.esa.cci.lc.util.LcHelper;
+import org.esa.cci.lc.util.PlanetaryGridName;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
 
 import java.io.File;
 
@@ -33,50 +36,51 @@ public class LcSubsetOp extends Operator implements Output {
     @Parameter(description = "The target directory.")
     private File targetDir;
 
-    @Parameter(description = "The western longitude.", interval = "[-180,180]", unit = "°")
+    @Parameter(description = "The western longitude.", interval = "[-180,360]", unit = "°")
     private Float west;
     @Parameter(description = "The northern latitude.", interval = "[-90,90]", unit = "°")
     private Float north;
-    @Parameter(description = "The eastern longitude.", interval = "[-180,180]", unit = "°")
+    @Parameter(description = "The eastern longitude.", interval = "[-180,360]", unit = "°")
     private Float east;
     @Parameter(description = "The southern latitude.", interval = "[-90,90]", unit = "°")
     private Float south;
 
     @Parameter(description = "A predefined set of north, east, south and west bounds.",
-               valueSet = {
-                       "NORTH_AMERICA", "CENTRAL_AMERICA", "SOUTH_AMERICA",
-                       "WESTERN_EUROPE_AND_MEDITERRANEAN", "ASIA", "AFRICA",
-                       "SOUTH_EAST_ASIA", "AUSTRALIA_AND_NEW_ZEALAND", "GREENLAND"
-               })
+            valueSet = {
+                    "NORTH_AMERICA", "CENTRAL_AMERICA", "SOUTH_AMERICA",
+                    "WESTERN_EUROPE_AND_MEDITERRANEAN", "ASIA", "AFRICA",
+                    "SOUTH_EAST_ASIA", "AUSTRALIA_AND_NEW_ZEALAND", "GREENLAND"
+            })
     private PredefinedRegion predefinedRegion;
 
 
     // for test cases
     Product subsetProduct;
     boolean writeProduct = true;
+    private File targetFile;
 
     @Override
     public void initialize() throws OperatorException {
-        if (!predefinedRegionIsSelected() && !userDefinedRegionIsSelected()) {
-            throw new OperatorException("Either predefined region or geographical bounds must be given.");
-        }
+        validateInputSettings();
         targetDir = LcHelper.ensureTargetDir(targetDir, getSourceProduct());
 
         String id = createId();
 
-        if (predefinedRegionIsSelected()) {
+        if (isPredefinedRegionSet()) {
             final PredefinedRegion r = predefinedRegion;
             north = r.getNorth();
             east = r.getEast();
             south = r.getSouth();
             west = r.getWest();
+            MetadataElement globalAttributes = getSourceProduct().getMetadataRoot().getElement(LcMapMetadata.GLOBAL_ATTRIBUTES_ELEMENT_NAME);
+            if (globalAttributes.getAttributeString("grid_name", "").startsWith("Regular gaussian grid")) {
+                // shift only for predefined regions
+                east = (east + 360) % 360;
+                west = (west + 360) % 360;
+            }
         }
-        MetadataElement globalAttributes = sourceProduct.getMetadataRoot().getElement(LcMapMetadata.GLOBAL_ATTRIBUTES_ELEMENT_NAME);
-        if (globalAttributes.getAttributeString("grid_name", "").startsWith("Regular gaussian grid")) {
-            east = (east + 360) % 360;
-            west = (west + 360) % 360;
-        }
-        subsetProduct = LcHelper.createProductSubset(sourceProduct, north, east, south, west, getRegionIdentifier());
+
+        subsetProduct = LcHelper.createProductSubset(getSourceProduct(), north, east, south, west, getRegionIdentifier());
 
         updateIdMetadataAttribute(id);
         final String formatName;
@@ -88,15 +92,52 @@ public class LcSubsetOp extends Operator implements Output {
             formatName = LcConditionNetCdf4WriterPlugIn.FORMAT_NAME;
         }
 
-        final File targetFile = new File(targetDir, id + ".nc");
+        if (targetFile == null) {
+            targetFile = new File(targetDir, id + ".nc");
+        }
+
         if (writeProduct) {
             GPF.writeProduct(subsetProduct, targetFile, formatName, false, ProgressMonitor.NULL);
         }
         setTargetProduct(new Product("dummy", "dummyType", 10, 10));
     }
 
+    private void validateInputSettings() {
+        if (!isPredefinedRegionSet() && !isUserDefinedRegionSet()) {
+            throw new OperatorException("Either predefined region or geographical bounds must be given.");
+        }
+
+        if (isRegularGuassianGrid() && getRegionIdentifier() != null) {
+            ReferencedEnvelope regionEnvelope = getRegionEnvelope();
+            double maxLon = regionEnvelope.getMaximum(0);
+            double minLon = regionEnvelope.getMinimum(0);
+            if (maxLon > 0 && minLon < 0) {
+                String msg = "The planetary grid '%s' can not be used in combination with a region " +
+                        "which crosses the prime meridian.";
+                throw new OperatorException(String.format(msg, PlanetaryGridName.REGULAR_GAUSSIAN_GRID));
+            }
+        }
+
+    }
+
+    private boolean isRegularGuassianGrid() {
+        MetadataElement globalAttributes = getSourceProduct().getMetadataRoot().getElement(LcMapMetadata.GLOBAL_ATTRIBUTES_ELEMENT_NAME);
+        return globalAttributes.getAttributeString("grid_name", "").startsWith("Regular gaussian grid");
+    }
+
+    private ReferencedEnvelope getRegionEnvelope() {
+        if (isPredefinedRegionSet()) {
+            return new ReferencedEnvelope(predefinedRegion.getEast(), predefinedRegion.getWest(),
+                                          predefinedRegion.getNorth(), predefinedRegion.getSouth(),
+                                          DefaultGeographicCRS.WGS84);
+        } else if (isUserDefinedRegionSet()) {
+            return new ReferencedEnvelope(east, west, north, south, DefaultGeographicCRS.WGS84);
+        }
+        return null;
+    }
+
     private void updateIdMetadataAttribute(String id) {
-        MetadataElement metadataRoot = sourceProduct.getMetadataRoot();
+        MetadataElement metadataRoot = getSourceProduct().getMetadataRoot();
         if (metadataRoot.containsElement(LcMapMetadata.GLOBAL_ATTRIBUTES_ELEMENT_NAME)) {
             MetadataElement globalAttributes = metadataRoot.getElement(LcMapMetadata.GLOBAL_ATTRIBUTES_ELEMENT_NAME);
             globalAttributes.setAttributeString("id", id);
@@ -104,7 +145,7 @@ public class LcSubsetOp extends Operator implements Output {
     }
 
     private String createId() {
-        MetadataElement metadataRoot = sourceProduct.getMetadataRoot();
+        MetadataElement metadataRoot = getSourceProduct().getMetadataRoot();
         if (metadataRoot.containsElement(LcMapMetadata.GLOBAL_ATTRIBUTES_ELEMENT_NAME)) {
             MetadataElement globalAttributes = metadataRoot.getElement(LcMapMetadata.GLOBAL_ATTRIBUTES_ELEMENT_NAME);
             final String id = globalAttributes.getAttributeString("id");
@@ -117,42 +158,46 @@ public class LcSubsetOp extends Operator implements Output {
     }
 
     private String getRegionIdentifier() {
-        if (predefinedRegionIsSelected()) {
+        if (isPredefinedRegionSet()) {
             return predefinedRegion.toString();
         } else {
             return "USER_REGION";
         }
     }
 
-    void setWest(float west) {
+    public void setWest(float west) {
         this.west = west;
     }
 
-    void setNorth(float north) {
+    public void setNorth(float north) {
         this.north = north;
     }
 
-    void setEast(float east) {
+    public void setEast(float east) {
         this.east = east;
     }
 
-    void setSouth(float south) {
+    public void setSouth(float south) {
         this.south = south;
     }
 
-    void setTargetDir(File targetDir) {
+    public void setTargetDir(File targetDir) {
         this.targetDir = targetDir;
     }
 
-    private boolean predefinedRegionIsSelected() {
-        return predefinedRegion != null;
+    public void setTargetFile(File targetFile) {
+        this.targetFile = targetFile;
     }
 
-    void setPredefinedRegion(PredefinedRegion predefinedRegion) {
+    public void setPredefinedRegion(PredefinedRegion predefinedRegion) {
         this.predefinedRegion = predefinedRegion;
     }
 
-    private boolean userDefinedRegionIsSelected() {
+    private boolean isPredefinedRegionSet() {
+        return predefinedRegion != null;
+    }
+
+    private boolean isUserDefinedRegionSet() {
         final boolean valid = north != null && east != null && south != null && west != null;
         if (valid) {
             if (west >= east) {
@@ -160,6 +205,12 @@ public class LcSubsetOp extends Operator implements Output {
             }
             if (north <= south) {
                 throw new OperatorException("North bound must be northern of south bound.");
+            }
+            if (isRegularGuassianGrid()) {
+                if (west < 0 || east < 0) {
+                    throw new OperatorException("West and east bound must be between " +
+                                                        "0 and 360 for regular gaussian grid.");
+                }
             }
         }
         return valid;
