5111938db91d5a3ea32e503dcb94f56b158227ee
Soman2 <roman.shevchuk@brockmann-consult.de>
2018-09-03 17:54:19 +0200

    temporary roll back to version 4.0, to have it as a maintance for LC.

--- lc-user-tools/src/main/java/org/esa/cci/lc/util/CdsVariableWriter.java
+++ lc-user-tools/src/main/java/org/esa/cci/lc/util/CdsVariableWriter.java
@@ -32,56 +32,118 @@ public class CdsVariableWriter extends ProfilePartIO {
         Array data ;
         final NFileWriteable writeable = ctx.getNetcdfFileWriteable();
         String path = p.getFileLocation().getAbsolutePath();
+        if (! path.endsWith(".tif") ) {
 
-        NetcdfFileWriter onlyReader = NetcdfFileWriter.openExisting(path);
-        List<Variable> list = onlyReader.getNetcdfFile().getVariables();
-        String[] listBands = p.getBandNames();
-        List bandArray = Arrays.asList(listBands);
-        for (Variable variable : list) {
-            String variableName = variable.getFullName();
-            if (!bandArray.contains(variableName) && !variableName.contains("burned_area_in_vegetation_class")) {
-                data = variable.read();
-                if (variableName.equals("lat_bnds")) {
-                    variableName = "lat_bounds";
-                }
-                else  if (variableName.equals("lon_bnds")) {
-                    variableName = "lon_bounds";
-                    IndexIterator iterator = data.getIndexIterator();
-                    for (int i = 0; iterator.hasNext(); i++) {
-                        data.setDouble(i, data.getDouble(i) + 180);
-                        iterator.next();
+            NetcdfFileWriter onlyReader = NetcdfFileWriter.openExisting(path);
+            List<Variable> list = onlyReader.getNetcdfFile().getVariables();
+            String[] listBands = p.getBandNames();
+            List bandArray = Arrays.asList(listBands);
+            for (Variable variable : list) {
+                String variableName = variable.getFullName();
+                if (!bandArray.contains(variableName) && !variableName.contains("burned_area_in_vegetation_class")) {
+                    data = variable.read();
+                    if (variableName.equals("lat_bnds")) {
+                        variableName = "lat_bounds";
+                    } else if (variableName.equals("lon_bnds")) {
+                        variableName = "lon_bounds";
+                        IndexIterator iterator = data.getIndexIterator();
+                        for (int i = 0; iterator.hasNext(); i++) {
+                            data.setDouble(i, data.getDouble(i) + 180);
+                            iterator.next();
+                        }
+                    } else if (variableName.equals("time_bnds")) {
+                        variableName = "time_bounds";
+                        double begTime = variable.read().getDouble(0);
+                        double endTime = variable.read().getDouble(1);
+                        data = Array.factory(DataType.DOUBLE, new int[]{1, 2}, new double[]{begTime, endTime});
+                    } else if (variable.getFullName().equals("lon")) {
+                        IndexIterator iterator = data.getIndexIterator();
+                        for (int i = 0; iterator.hasNext(); i++) {
+                            data.setDouble(i, data.getDouble(i) + 180);
+                            iterator.next();
+                        }
                     }
-                }
-                else if (variableName.equals("time_bnds")) {
-                    variableName = "time_bounds";
-                    double begTime = variable.read().getDouble(0);
-                    double endTime = variable.read().getDouble(1);
-                    data = Array.factory(DataType.DOUBLE, new int[]{1, 2}, new double[]{begTime, endTime});
-                }
-                else if (variable.getFullName().equals("lon")) {
-                    IndexIterator iterator = data.getIndexIterator();
-                    for (int i = 0; iterator.hasNext(); i++) {
-                        data.setDouble(i, data.getDouble(i) + 180);
-                        iterator.next();
-                    }
-                }
 
-                try {
-                    Object temp = data.get1DJavaArray(ctx.getNetcdfFileWriteable().findVariable(variableName).getDataType());
-                    data = Array.makeFromJavaArray(temp);
-                    data = data.reshape(variable.getShape());
-                    writeable.getWriter().write(writeable.getWriter().findVariable(variableName), data);
-                } catch (InvalidRangeException e) {
+                    try {
+                        Object temp = data.get1DJavaArray(ctx.getNetcdfFileWriteable().findVariable(variableName).getDataType());
+                        data = Array.makeFromJavaArray(temp);
+                        data = data.reshape(variable.getShape());
+                        writeable.getWriter().write(writeable.getWriter().findVariable(variableName), data);
+                    } catch (InvalidRangeException e) {
+                    }
                 }
             }
+            onlyReader.close();
+
+            if (p.getMetadataRoot().getElement("global_attributes").getAttributeString("type").equals("ESACCI-LC-L4-LCCS-Map-300m-P1Y")) {
+                lcLatLonBoundsWriter(ctx, p);
+                timeWriter(ctx, p);
+            }
         }
-        onlyReader.close();
-        if (p.getMetadataRoot().getElement("global_attributes").getAttributeString("type").equals("ESACCI-LC-L4-LCCS-Map-300m-P1Y")) {
-            lcLatLonBoundsWriter(ctx,p);
-            timeWriter(ctx,p);
+        else {
+            Double latMin = p.getMetadataRoot().getElement("global_attributes").getAttributeDouble("geospatial_lat_min");
+            Double latMax = p.getMetadataRoot().getElement("global_attributes").getAttributeDouble("geospatial_lat_max");
+            Double lonMin = p.getMetadataRoot().getElement("global_attributes").getAttributeDouble("geospatial_lon_min");
+            Double lonMax = p.getMetadataRoot().getElement("global_attributes").getAttributeDouble("geospatial_lon_max");
+            lcLatLonCustomBoundsWriter(ctx,p,lonMin,lonMax,latMin,latMax);
         }
     }
 
+    private void lcLatLonCustomBoundsWriter(ProfileWriteContext ctx, Product p, Double minLon, Double maxLon, Double minLat, Double maxLat) throws IOException {
+        Array data;
+        final NFileWriteable writeable = ctx.getNetcdfFileWriteable();
+        int numLatPixels = p.getSceneRasterHeight();
+        double divider = numLatPixels / (maxLat - minLat);
+        double step = 1 / divider;
+
+        double[] latArray = new double[2 * numLatPixels];
+        double[] lat = new double[ numLatPixels];
+        int j = 0;
+        for (int i = 0; i < numLatPixels; i += 1) {
+            latArray[j] = minLat + (i * step);
+            latArray[j + 1] = minLat + (i + 1) * step;
+            j += 2;
+            lat[i]=minLat + (i * step);
+        }
+        data = Array.factory(DataType.DOUBLE, new int[]{numLatPixels, 2}, latArray);
+        try {
+            data = data.flip(0);
+            data = data.flip(1);
+            writeable.getWriter().write("lat_bounds", data);
+        } catch (InvalidRangeException e) {
+        }
+        data = Array.factory(DataType.DOUBLE,new int[]{numLatPixels, 1}, lat);
+        try {
+            writeable.getWriter().write("lat", data);
+        }
+        catch (InvalidRangeException e) {
+        }
+        //lon part
+        int numLonPixels = p.getSceneRasterWidth();//129600;
+        divider = numLonPixels / (maxLon -minLon);
+        step = 1 / divider;
+        double[] lonArray = new double[2 * numLonPixels];
+        double[] lon = new double[ numLonPixels];
+        j = 0;
+        for (int i = 0; i < numLonPixels; i += 1) {
+            lonArray[j] = minLon + (i * step);
+            lonArray[j + 1] = minLon + (i + 1) * step;
+            lon[i]=minLon + (i * step);
+            j += 2;
+        }
+        data = Array.factory(DataType.DOUBLE, new int[]{numLonPixels, 2}, lonArray);
+        try {
+            writeable.getWriter().write("lon_bounds", data);
+        }
+        catch (InvalidRangeException e) {
+        }
+        data = Array.factory(DataType.DOUBLE,new int[]{numLatPixels, 1}, lon);
+        try {
+            writeable.getWriter().write("lon", data);
+        }
+        catch (InvalidRangeException e) {
+        }
+    }
 
 
     private void lcLatLonBoundsWriter(ProfileWriteContext ctx, Product p) throws IOException {
@@ -91,7 +153,7 @@ public class CdsVariableWriter extends ProfilePartIO {
 
         double divider = numLatPixels / 180; //360; //number of pixels in 1 degree
         double step = 1 / divider;
-        int minLat = -90;
+        double minLat = -90;
 
         //lat part
         double[] latArray = new double[2 * numLatPixels];
@@ -112,7 +174,7 @@ public class CdsVariableWriter extends ProfilePartIO {
         int numLonPixels = p.getSceneRasterWidth();//129600;
         divider = numLonPixels / 360; //360; //number of pixels in 1 degree
         step = 1 / divider;
-        int minLon = 0;
+        double minLon = 0;
         double[] lonArray = new double[2 * numLonPixels];
 
         j = 0;
@@ -128,6 +190,9 @@ public class CdsVariableWriter extends ProfilePartIO {
         catch (InvalidRangeException e) {
         }
     }
+
+
+
     private void timeWriter (ProfileWriteContext ctx, Product p) throws IOException {
         Array data;
         String path = p.getFileLocation().getAbsolutePath();
